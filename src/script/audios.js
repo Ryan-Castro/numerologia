8e9062271aa299f5cfe954e8b3eb79c8833a6549
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2023-06-29 16:48:11 -0300

    dia4

---
+++ src/script/audios.js
@@ -0,0 +1,112 @@
+function playPresentationDestiny(){
+    if(checkInputs()){
+        return
+    }
+    let [selectedDay, selectedMonth, selectedYear] = getDate()
+    document.querySelector("#analizingName").innerHTML = _name
+    document.querySelector("#analizingDate").innerHTML = `${selectedDay} / ${selectedMonth} / ${selectedYear}`
+    showSec('secAnalizing')
+    audio.src = "./src/audios/presentationOnDestiny.mp3"
+    audio.play()
+    audio.addEventListener("ended", presDes)
+    function presDes(){
+        presentationDestiny = true
+        audio.removeEventListener("ended", presDes)
+        starAnalyzing()
+    }
+}
+function playDestiny(){
+    let numberDestiny = calcDestiny()
+    audio.src = `./src/audios/destiny/destino${numberDestiny}.mp3`
+    audio.play()
+    audio.addEventListener("ended", Dest)
+    function Dest(){
+        destiny = true
+        audio.removeEventListener("ended", Dest)
+        starAnalyzing()
+    }
+}
+function playIntroductionExpression(){
+    audio.src = `./src/audios/introductionExpression.mp3`
+    audio.play()
+    audio.addEventListener("ended", intExpre)
+    function intExpre(){
+        introductionExpression = true
+        showSec('secFullName')
+        audio.removeEventListener("ended", intExpre)
+    }
+}
+function playPresentationExpression(){
+    showSec('secAnalizing')
+    audio.src = "./src/audios/presentationExpression.mp3"
+    audio.play()
+    audio.addEventListener("ended", presExprex)
+    function presExprex(){
+        presentationExpression = true
+        audio.removeEventListener("ended", presExprex)
+        starAnalyzing()
+    }
+
+}
+function playExpression(){
+    let numberExpression = calcExpression()
+    audio.src = `./src/audios/expression/expressao${numberExpression}.mp3`
+    audio.play()
+    audio.addEventListener("ended", expres)
+    function expres(){
+        expression = true
+        audio.removeEventListener("ended", expres)
+        starAnalyzing()
+    }
+}
+function playintroductionMotivation(){
+    audio.src = `./src/audios/introductionMotivation.mp3`
+    audio.play()
+    audio.addEventListener("ended", intMot)
+    function intMot(){
+        introductionMotivation = true
+        showSec('secEmail')
+        audio.removeEventListener("ended", intMot)
+    }
+}
+function playPresentetionMotivation(){
+    showSec('secAnalizing')
+    audio.src = "./src/audios/presentationMotivation.mp3"
+    audio.play()
+    audio.addEventListener("ended", presMot)
+    function presMot(){
+        presentationMotivation = true
+        audio.removeEventListener("ended", presMot)
+        starAnalyzing()
+    }
+}
+function playMotivarion(){
+    let numberMotivation = calcMotivation()
+    audio.src = `./src/audios/motivation/motivacao${numberMotivation}.mp3`
+    audio.play()
+    audio.addEventListener("ended", mot)
+    function mot(){
+        motivarion = true
+        audio.removeEventListener("ended", mot)
+        starAnalyzing()
+    }
+
+}
+function playEnded(){
+    audio.src = `./src/audios/end1.mp3`
+    audio.play()
+    audio.addEventListener("ended", audio2)
+    function audio2(){
+        audio.removeEventListener("ended", audio2)
+        audio.src = `./src/audios/end2.mp3`
+        audio.play()
+        audio.addEventListener("ended", audio3)
+    }
+    function audio3(){
+        audio.removeEventListener("ended", audio3)
+        ended = true
+        showSec('secBuy')
+        audio.src = `./src/audios/end3.mp3`
+        audio.play()
+    }
+}
--- src/script/script.js
+++ src/script/script.js
@@ -1,59 +1,183 @@
 let synth = new SpeechSynthesisUtterance();
 let audio = document.querySelector("#mainAudio")
-let presetention = false
-let enderEmail = false
-let showEmail = false
+let _name = ""
+let _fullName = ""
+let selectedName = false
+let presentationDestiny = false
+let presentationExpression = false
+let presentationMotivation = false
+let destiny = false
+let expression = false
+let motivarion = false
+let introductionExpression = false
+let introductionMotivation = false
 let ended = false
-audio.addEventListener("ended", ()=>{
-    starAnalyzing()
-})
+
+
 
 function starAnalyzing() {
-    let _name = document.querySelector("#name").value
-    let numberCab = analyzingName(_name)
-    if(!presetention){
-        document.querySelector("#secInputName").style.display = "none"
-        document.querySelector("#secAnalizing").style.display = "flex"
-        document.querySelector("#analizingName").innerHTML = _name
-        presetention = true
-        speak(`./src/audios/presentation.mp3`)
+    if(!selectedName){
+        _name = document.querySelector("#inputName").value
+        if(_name == ""){
+            alert("digite seu nome")
+            return
+        }
+        selectedName = true
+        showSec('secInputDate')
         return
     }
-    if(!enderEmail){
-        speak(`./src/audios/inputEmail.mp3`)
-        enderEmail = true
+    if(!presentationDestiny){
+        playPresentationDestiny()
         return
     }
-    if(!showEmail){
-        document.querySelector("#secAnalizing").style.display = "none"
-        document.querySelector("#secEmail").style.display = "flex"
-        showEmail = true
+    if(!destiny){
+        playDestiny()
+        return
+    }
+    if(!introductionExpression){
+        playIntroductionExpression()
+        return
+    }
+    if(!presentationExpression){
+        _fullName = document.querySelector("#inputFullName").value
+        if(_fullName == ""){
+            alert("digite seu nome")
+            return
+        }
+        playPresentationExpression()
+        return
+    }
+    if(!expression){
+        playExpression()
+        return
+    }
+    if(!introductionMotivation){
+        playintroductionMotivation()
+        return
+    }
+    if(!presentationMotivation){
+        playPresentetionMotivation()
+        return
+    }
+    if(!motivarion){
+        playMotivarion()
         return
     }
     if(!ended){
-        alert(`aqui vai ter mais um audio`)
-        document.querySelector("#secEmail").style.display = "none"
-        document.querySelector("#secBuy").style.display = "flex"
-        ended = true
+        playEnded()
+        return
     }
 
 } 
-function speak(linkAudio){
-    audio.src = linkAudio
-    
-    audio.play()
+
+
+function getDate(){
+    let selecDay = document.querySelector("#inputDay")
+    let selecMonth = document.querySelector("#inputMonth")
+    let selecYear = document.querySelector("#inputYear")
+    let selectedDay = selecDay.options[selecDay.selectedIndex].value
+    let selectedMonth = selecMonth.options[selecMonth.selectedIndex].value
+    let selectedYear = selecYear.options[selecYear.selectedIndex].value
+    return [selectedDay, selectedMonth, selectedYear]
 }
 
-function analyzingName(name){
-    let sumName = 0
-    name.toLowerCase().split("").forEach(letter=>{
-        sumName += tableCabalistic[letter]
-    })
-    while (sumName>9) {
-        let digito1 = sumName.toString()[sumName.toString().length-1]
-        sumName = Number(sumName) - Number(digito1)
-        sumName = sumName / 10
-        sumName = Number(sumName) + Number(digito1)
+
+function checkInputs(){
+    let [selectedDay, selectedMonth, selectedYear] = getDate()
+    if(selectedDay == ""){
+        alert("Coloque o seu dia de nascimento")
+        return true
     }
-    return sumName
+    if(selectedMonth == ""){
+        alert("Coloque o seu mes de nascimento")
+        return true
+    }
+    if(selectedYear == ""){
+        alert("Coloque o seu ano de nascimento")
+        return true
+    }
+    return false
 }
+
+
+function calcDestiny(){
+    let [selectedDay, selectedMonth, selectedYear] = getDate()
+    let sDay = synthesizeNumber(selectedDay)
+    let sMonth = synthesizeNumber(selectedMonth)
+    let sYear = synthesizeNumber(selectedYear)
+    let sumNumber = sDay + sMonth + sYear
+    let sDestiny = synthesizeNumber(sumNumber)
+    return sDestiny
+}
+
+function calcExpression(){
+    let nameFull = _fullName.replaceAll(" ", "").replaceAll("a", "").replaceAll("e", "").replaceAll("i", "").replaceAll("o", "").replaceAll("u", "")
+    let sumNumber = 0
+    nameFull.split("").forEach(letter=>{
+        sumNumber += tableCabalistic[letter]
+    })
+    return synthesizeNumber(sumNumber)
+}
+
+function calcMotivation(){
+    let nameFull = _fullName.replaceAll(" ", "")
+    let sumNumber = 0
+    nameFull.split("").forEach(letter=>{
+        switch (letter) {
+            case "a": sumNumber += tableCabalistic[letter]; break;
+            case "e": sumNumber += tableCabalistic[letter]; break;
+            case "i": sumNumber += tableCabalistic[letter]; break;
+            case "o": sumNumber += tableCabalistic[letter]; break;
+            case "u": sumNumber += tableCabalistic[letter]; break;
+            default:
+                break;
+        }
+        
+    })
+    return synthesizeNumber(sumNumber)
+}
+
+function synthesizeNumber(number){
+    while (number>9) {
+        let digito1 = number.toString()[number.toString().length-1]
+        number = Number(number) - Number(digito1)
+        number = number / 10
+        number = Number(number) + Number(digito1)
+    }
+    return number
+}
+
+function showSec(section){
+    hideSec()
+    document.querySelector("#"+section).style.display = "flex"
+}
+function hideSec(){
+    document.querySelectorAll("section").forEach(element=>{
+        element.style.display = "none"
+    })
+}
+function setCalendar(){
+    let months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Desembro"]
+    document.querySelector("#inputDay").innerHTML = `<option value="">Coloque um Dia</option>`
+    document.querySelector("#inputMonth").innerHTML = `<option value="">Coloque um Mes</option>`
+    document.querySelector("#inputYear").innerHTML = `<option value="">Coloque um Ano</option>`
+    for(let day=1;day<32;day++){
+        if(day<10){
+            document.querySelector("#inputDay").innerHTML += `<option value='0${day}'>0${day}</option>`
+        } else {
+            document.querySelector("#inputDay").innerHTML += `<option value='${day}'>${day}</option>`
+        }
+    }
+    for(let month=1;month<12;month++){
+        if(month<10){
+            document.querySelector("#inputMonth").innerHTML += `<option value='0${month}'>${months[month-1]}</option>`
+        } else {
+            document.querySelector("#inputMonth").innerHTML += `<option value='${month}'>${months[month-1]}</option>`
+        }
+    }
+    for(let year=0;year<100;year++){
+        document.querySelector("#inputYear").innerHTML += `<option value=${(2023 - year)}>${(2023 - year)}</option>`
+    }
+}
+showSec("secInputName")
+setCalendar()
